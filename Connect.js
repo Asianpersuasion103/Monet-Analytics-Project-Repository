ab906b8a8a92e9f3c5ee17b0aac07ff660c18561
Drake Hashimoto <th3speedforc3@outlook.com>
2026-08-10 12:10:24 -0700

    Added webdev files

---
+++ Connect.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const testButton = document.querySelector("#testButton");
+
+testButton.addEventListener("click", () => {
+  console.log("JavaScript is connected!");
+  alert("Everything is connected!");
+});
