2d905f763aa76b1e8835822fd37531ddd8ff73d7
Drake Hashimoto <th3speedforc3@outlook.com>
2026-08-10 18:15:42 -0700

    Added Interviewer & Interviewee html files

--- Connect.js
+++ Connect.js
@@ -5,10 +5,14 @@ const testButton2 = document.querySelector("#intervieweeButton");
 
 testButton1.addEventListener("click", () => {
   console.log("Working!");
+  window.location.href = "interviewer.html";
+
   /*alert("Let's prepare your meeting!");*/
 });
 
 testButton2.addEventListener("click", () => {
   console.log("Working!");
+  window.location.href = "interviewee.html";
+
   /*alert("Get ready for your interview!");*/
 });
